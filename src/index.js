ef5a5eb5e0212541bac503b606158c22d05f21b4
Grant Reighard <grant.reighard@gmail.com>
2025-11-07 15:46:04 -0500

    adds posthog

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PostHogProvider } from "posthog-js/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_POSTHOG_KEY}
+      options={{
+        api_host: process.env.REACT_APP_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+        debug: process.env.NODE_ENV === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
 
